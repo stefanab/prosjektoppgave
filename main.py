9fb92511f6a4d4810ee8a32218a23a84c3e27f0e
stefanab <stefanasborg@gmail.com>
2017-10-18 10:49:52 +0200

    Filled out main to follow a straight line

--- main.py
+++ main.py
@@ -15,23 +15,51 @@ def cleanUp():
 
 def main():
     motors    = Motors()
-    infrared = ReflectanceSensors()
+    infrared  = ReflectanceSensors()
+	(infrared.update())
     print("setup")
         
-##	# To exit the program properly on keyboardinterrupt
-##    def signal_handler(signal, frame):
-##        motors.stop()
-##        GPIO.cleanup()
-##        sys.exit(0)
-##    signal.signal(signal.SIGINT, signal_handler)
-    motors.forward(dur=1)
-    
-    (infrared.update())
+	# To exit the program properly on keyboardinterrupt
+    def signal_handler(signal, frame):
+       motors.stop()
+       GPIO.cleanup()
+       sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    ##motors.forward(dur=1)
+    iteration = 0
+	while(!isGoal(infrared) && iteration < 5):
+        (infrared.update())
+	    motors.forward(dur=.3)
+		sleep(1)
+		iteration += 1
+	   
     
 ##    motors.stop()
   ##  GPIO.cleanup()
 ##    
     
+def isGoal(infrared):
+	values = infrared.get_value()
+	darks = 0
+	for i in range(len(values)):
+        value = values[i]
+        if value > 0.2:
+            darks += 1
+
+        if value > 0.05:
+            avg += (value * i * 1000);
+            vSum += value;
+
+			
+	print("darks " + str(darks))
+	
+	if(darks >= 6):
+		return true
+	else:
+		return false
+	
+	
+	
 if __name__ == "__main__":
     main()
 	
