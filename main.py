bec4706c07edc7c6ebd5652157e0c16eb4990fd9
stefanab <stefanasbo@gmail.com>
2017-10-18 08:23:06 +0000

    Some more files added

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from motors import Motors
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 from time import sleep
 import signal
 import sys
-from reflectance_sensors import ReflectanceSensors
-
-main():
-	motor    = motors.Motors()
-	infrared = reflectance_sensors.ReflectanceSensors()
+from robot.motors import Motors
+from robot.reflectance_sensors import ReflectanceSensors
+def main():
+    motors    = Motors()
+    infrared = ReflectanceSensors()
+    print("setup")
 	
-	
-	# To exit the program properly on keyboardinterrupt
-    def signal_handler(signal, frame):
-        motors.stop()
-        GPIO.cleanup()
-        sys.exit(0)
-    signal.signal(signal.SIGINT, signal_handler)
+##	# To exit the program properly on keyboardinterrupt
+##    def signal_handler(signal, frame):
+##        motors.stop()
+##        GPIO.cleanup()
+##        sys.exit(0)
+##    signal.signal(signal.SIGINT, signal_handler)
+    motors.forward(dur=1)
+    
+    (infrared.update())
+    
+##    motors.stop()
+  ##  GPIO.cleanup()
+##    
+    
+if __name__ == "__main__":
+    main()
 	
 	
