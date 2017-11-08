67838c5cb576bab098aa23ca39a3a41dc7dee010
stefanab <stefanasbo@gmail.com>
2017-11-08 15:40:27 +0000

    Added neural net with camera

--- neuralnets.py
+++ neuralnets.py
@@ -54,10 +54,10 @@ def conv_neural_network_model1(name="conv_neural_network_model1", shape=None, im
 
     return model
 
-def conv_reflectance_neural_network_model1(n_actions=3, name="conv_neural_network_model1", shape=None, imconstpar=constantParametersImage(), netconstpar=constantParametersNetwork()):
+def conv_reflectance_neural_network_model1(n_actions=3, name="conv_ref_neural_network_model1", shape=None, imconstpar=constantParametersImage(), netconstpar=constantParametersNetwork()):
 
     convnet = input_data(shape=[None, imconstpar.height, imconstpar.width, imconstpar.channels], name='image_input')
-    reflectance = tflearn.input_data(shape=[None, 1, 6, 1], name="reflectance_input")
+    reflectance = tflearn.input_data(shape=[None, 6], name="reflectance_input")
 
     convnet = conv_2d(convnet, 32, 8, strides=4, activation='relu', name='conv1')
     #convnet = max_pool_2d(convnet, 2)
@@ -74,11 +74,11 @@ def conv_reflectance_neural_network_model1(n_actions=3, name="conv_neural_networ
 
     convnet = fully_connected(convnet, 8, activation='softmax', name='fc2')
 
-    merged_net = merge([convnet, reflectance], mode='concat', axis=0)
+    merged_net = merge([convnet, reflectance], mode='concat', axis=1)
     #convnet = regression(convnet, optimizer='SGD', loss='categorical_crossentropy', name='targets')
     merged_net = fully_connected(merged_net, 128, activation='sigmoid', name='m_fc1')
     merged_net = fully_connected(merged_net, n_actions, activation='linear', name='m_fc2')
-    merged_net = regression(merged_net, optimizer='adam', loss='categorical_crossentropy', name='targets')
+    merged_net = regression(merged_net, optimizer='adam',learning_rate=0.00001, loss='categorical_crossentropy', name='targets')
     model = tflearn.DNN(merged_net)
 
     return model, name
